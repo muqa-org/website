6764bbc4ffa7c568bb589deae2c3a11aa2eede3e
kkatusic <kkatusic@gmail.com>
2024-10-04 10:31:03 +0200

    Involved finished

--- src/app/components/homepage/HomepageInvolved.tsx
+++ src/app/components/homepage/HomepageInvolved.tsx
@@ -2,7 +2,10 @@
 
 import { useFormState } from 'react-dom';
 
+import { involvedAction } from '@/app/actions';
+
 import Container from '@/app/components/Container';
+import InvolvedFormButton from './InvolvedFormButton';
 
 type MessageType = {
 	key: string;
@@ -26,11 +29,49 @@ export const HomepageInvolved = () => {
 	return (
 		<div className='pb-16 pt-10'>
 			<Container>
-				<div className='px-16'>
-					<h2 className='mb-8 text-center text-4xl font-medium leading-10 text-primaryBlack'>
+				<div className='px-8'>
+					<h2 className='mb-20 text-center text-4xl font-medium leading-10 text-primaryBlack'>
 						Get Involved
 					</h2>
-					<div className='flex justify-between px-8 py-12'>fsdfds</div>
+					<div className='relative rounded-[13px] rounded-bl-none bg-[#E8F7FB] px-10 py-9'>
+						<form
+							action={formAction}
+							className='flex flex-row flex-wrap justify-between'
+						>
+							<div className='w-[48%]'>
+								<textarea
+									className='h-36 w-full rounded-md bg-white p-4 text-base text-black placeholder-gray-600'
+									placeholder='Leave us a message'
+								></textarea>
+							</div>
+							<div className='flex w-[48%] flex-col'>
+								<input
+									className='w-full rounded-md bg-white p-4 text-base text-black placeholder-gray-600'
+									placeholder='Email'
+								/>
+								<input
+									className='mt-auto w-full rounded-md bg-white p-4 text-base text-black placeholder-gray-600'
+									placeholder='Name'
+								/>
+							</div>
+							<div className='w-full pt-8 pl-4'>
+								<InvolvedFormButton />
+								{!state.status && state.message.length > 0 && (
+									<div className='mt-3 block rounded-lg bg-red-500 p-4 text-white'>
+										ERROR
+									</div>
+								)}
+							</div>
+						</form>
+						<div
+							className='absolute bottom-[-30px] left-0 h-20 w-20'
+							style={{
+								backgroundImage: 'url(/images/down-mark.svg)',
+								backgroundPosition: 'bottom left',
+								backgroundRepeat: 'no-repeat',
+							}}
+						></div>
+					</div>
 				</div>
 			</Container>
 		</div>
